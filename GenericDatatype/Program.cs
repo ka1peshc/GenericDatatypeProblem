99689fe5e519cb52fe8ba69ccc7e2449a27a730c
ka1peshc <kalpesh111997@gmail.com>
2021-09-16 17:57:57 +0530

    Pass generic array and sort and fetch max number

--- GenericDatatype/Program.cs
+++ GenericDatatype/Program.cs
@@ -14,10 +14,23 @@ namespace GenericDatatype
             GenericType gt = new GenericType();
             float maxNumber = gt.FindMax(num1, num2, num3);
             Console.WriteLine("Max number is {0}", maxNumber);
-            */
+            
             GenericType gt = new GenericType();
             string maxString = gt.FindMax("Eleven","thirtyThree","fourtyFour");
             Console.WriteLine("Max string is {0}", maxString);
+            */
+            int[] intArr = { 1, 3, 19, 22, 12 };
+            GenericType<int> generic = new GenericType<int>(intArr);
+            generic.PrintMaxValue();
+
+            string[] stringArr = { "one","sixteen", "three" };
+            GenericType<string> generic1 = new GenericType<string>(stringArr);
+            generic1.PrintMaxValue();
+
+            double[] doubleArr = { 111.1, 14.2, 557.2 };
+            GenericType<Double> genricDouble = new GenericType<double>(doubleArr);
+            genricDouble.PrintMaxValue();
+
         }
     }
 }
