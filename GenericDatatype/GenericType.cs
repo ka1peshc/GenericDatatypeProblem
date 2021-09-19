a3ed74b23b5cd9e94a55c6f6d4f0a292c4ed5101
ka1peshc <kalpesh111997@gmail.com>
2021-09-19 22:44:02 +0530

    Added comments in code for future understanding

--- GenericDatatype/GenericType.cs
+++ GenericDatatype/GenericType.cs
@@ -6,21 +6,37 @@ namespace GenericDatatype
     class GenericType<T> where T : IComparable
     {
         public T[] value;
-
+        /// <summary>
+        /// Class GenericType Constructor 
+        /// </summary>
+        /// <param name="value">array</param>
         public GenericType(T[] value)
         {
             this.value = value;
         }
+        /// <summary>
+        /// Sort array using inbuilt method
+        /// </summary>
+        /// <param name="value">Array</param>
+        /// <returns>Sorted array</returns>
         public T[] Sort(T[] value)
         {
             Array.Sort(value);
             return value;
         }
+        /// <summary>
+        /// Fetch last element from sorted array
+        /// </summary>
+        /// <param name="values">Array</param>
+        /// <returns>Array last element</returns>
         public T maxValue(params T[] values)
         {
             var sortedValue = Sort(values);
             return sortedValue[^1];
         }
+        /// <summary>
+        /// Display maximum value in the array
+        /// </summary>
         public void PrintMaxValue()
         {
             var max = maxValue(this.value);
--- GenericDatatype/Program.cs
+++ GenericDatatype/Program.cs
@@ -6,19 +6,7 @@ namespace GenericDatatype
     {
         static void Main(string[] args)
         {
-            /*
-            Console.WriteLine("Enter three number");
-            float num1 = float.Parse(Console.ReadLine());
-            float num2 = float.Parse(Console.ReadLine());
-            float num3 = float.Parse(Console.ReadLine());
-            GenericType gt = new GenericType();
-            float maxNumber = gt.FindMax(num1, num2, num3);
-            Console.WriteLine("Max number is {0}", maxNumber);
             
-            GenericType gt = new GenericType();
-            string maxString = gt.FindMax("Eleven","thirtyThree","fourtyFour");
-            Console.WriteLine("Max string is {0}", maxString);
-            */
             int[] intArr = { 1, 3, 19, 22, 12 };
             GenericType<int> generic = new GenericType<int>(intArr);
             generic.PrintMaxValue();
